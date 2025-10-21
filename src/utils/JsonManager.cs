1c5565d25043bf7b13d38947fab28663e7bf355b
KeDeX <albers.mark02@gmail.com>
2025-10-21 23:36:49 +0200

    add file to manage json files with load and save method

---
+++ src/utils/JsonManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ActiveTwitch.src.utils
+{
+    public static class JsonManager
+    {
+        public static T? LoadFromFile<T>(string filePath) where T : class
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    var json = File.ReadAllText(filePath, Encoding.UTF8);
+                    var obj = System.Text.Json.JsonSerializer.Deserialize<T>(json);
+                    return obj;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debugger.Debug($"[JsonManager::LoadFromFile]Error: {ex.Message}");
+            }
+            return null;
+        }
+        
+        public static bool SaveToFile<T>(string filePath, T obj) where T : class
+        {
+            try
+            {
+                var json = System.Text.Json.JsonSerializer.Serialize(obj, new System.Text.Json.JsonSerializerOptions
+                {
+                    WriteIndented = true
+                });
+                File.WriteAllText(filePath, json, Encoding.UTF8);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debugger.Debug($"[JsonManager::SaveToFile]Error: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
