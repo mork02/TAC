f367ef662c93f2cfcab142956f7cf1c98edfda4d
KeDeX <albers.mark02@gmail.com>
2025-10-21 23:36:04 +0200

    add simple debugger in logs

---
+++ src/utils/Debugger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace ActiveTwitch.src.utils
+{
+    public static class Debugger
+    {
+        private static readonly string _logDir = Path.Combine(AppContext.BaseDirectory, "logs");
+        private static readonly string _filePath = Path.Combine(_logDir, $"log-{DateTime.UtcNow:yyyy-MM-dd}.txt");
+
+        public static void Debug(string message)
+        {
+            try
+            {
+                Directory.CreateDirectory(_logDir);
+
+                string logLine = $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {message}";
+                File.AppendAllText(_filePath, logLine + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[Logger Error] {ex.Message}");
+            }
+        }
+    }
+}
