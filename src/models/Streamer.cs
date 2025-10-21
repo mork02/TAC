efd62d6bb9ad05aa1e632e53947c04ac9526b5fd
KeDeX <albers.mark02@gmail.com>
2025-10-21 23:35:15 +0200

    rename

---
+++ src/models/Streamer.cs
@@ -0,0 +1,35 @@
+﻿namespace ActiveTwitch.Models
+{
+    public record Streamer (
+        string Id,
+        string Login,
+        string DisplayName)
+    {
+        public bool IsLive { get; set; }
+        public DateTimeOffset LastStatusChangeUtc { get; set; } = DateTimeOffset.UtcNow;
+        public DateTimeOffset? LiveStartedAtUtc { get; set; }
+
+        public string? Title { get; set; }
+        public string? CategoryId { get; set; }
+        public string? CategoryName { get; set; }
+
+        public Uri Url => new($"https://twitch.tv/{Login}");
+
+        public void ApplyOnline(DateTimeOffset startedAt, string? title = null, string? categoryId = null, string? categoryName = null)
+        {
+            IsLive = true;
+            LiveStartedAtUtc = startedAt;
+            Title = title;
+            CategoryId = categoryId;
+            CategoryName = categoryName;
+            LastStatusChangeUtc = DateTimeOffset.UtcNow;
+        }
+
+        public void ApplyOffline()
+        {
+            IsLive = false;
+            LiveStartedAtUtc = null;
+            LastStatusChangeUtc = DateTimeOffset.UtcNow;
+        }
+    }
+}
